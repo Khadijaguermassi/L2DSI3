7f79b8120a949dac570312d752692e33c7eb4e21
Khadija Guermassi <guermassikhadija31@gmail.com>
2023-09-25 11:11:00 +0100

    V1: ex et test  de atelier1

---
+++ atelier1/ex.php
@@ -0,0 +1,59 @@
+<?php
+$j=rand(1,7);
+switch($j) {
+    case 1:
+    echo "Dimanche";
+    break;
+     case 2:
+    echo "Lundi";
+    break;
+     case 3:
+    echo "Mardi";
+    break;
+     case 4:
+    echo "Mercredi";
+    break;
+     case 5:
+    echo "Jeudi";
+    break;
+     case 6:
+    echo "Vendredi";
+    break;
+     case 7:
+    echo "Samedi";
+    break;
+    default: 
+    echo "pas valide";
+}
+
+
+
+
+$x=rand(0,4);
+switch($x) {
+    case 0:
+    echo "inferieur à 3";
+    break;
+    case 1:
+    echo "inferieur à 3";
+    break;
+    case 2:
+    echo "inferieur à 3";
+    break;
+    case 3:
+    echo "égale à 3 ";
+    break ;
+    case 4:
+    echo "superieur à 3 ";
+    break;
+    default: 
+    echo "non valide";
+    
+
+}
+
+
+
+
+
+
